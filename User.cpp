1c3315f1541d305b7ed699062cd503fc26fd89b3
Hamed70HMD <Hamed70HMD@gmail.com>
2019-05-22 01:24:43 -0700

    hash function moved to Interface class

--- User.cpp
+++ User.cpp
@@ -1,12 +1,30 @@
 #include "User.h"
 
+using namespace std;
+
 User::User(int _id) { id = _id; }
 
 void User::set_username(std::string _username) { username = _username; }
-void User::set_password(std::string _password) { password = std::to_string(hash(_password)); }
-void User::set_email(std::string _email) { email = _email; }
-void User::set_age(std::string _age) { age = _age; }
+void User::set_password(std::string _password) { password = _password; }
+void User::set_email(std::string _email)
+{
+    if (_email.find('@')==string::npos)
+        throw WrongInput();
+    email = _email;
+}
+void User::set_age(std::string _age)
+{
+    if (std::stoi(_age) < 1)
+        throw WrongInput();
+    age = _age;
+}
 void User::set_publisher(bool _publisher) { publisher = _publisher; }
+void User::set_balance(int _amount)
+{
+    if (_amount < 1)
+        throw WrongInput();
+    balance += _amount;
+}
 
 int User::get_id() { return id; }
 std::string User::get_username() { return username; }
@@ -14,12 +32,3 @@ std::string User::get_password() { return password; }
 std::string User::get_email() { return email; }
 std::string User::get_age() { return age; }
 bool User::get_publisher() { return publisher; }
-
-long long User::hash(std::string pass)
-{
-    const int mod = 1e9 + 7;
-    long long ans = 0;
-    for(int i = 0 ;i< pass.size();i++)
-        ans = ((ans * 313) + pass[i]) % mod;
-    return ans;
-}
--- User.h
+++ User.h
@@ -3,11 +3,10 @@
 
 #include <iostream>
 #include <string>
-#include <sstream>
 #include <vector>
-#include <fstream>
-#include <cstdlib>
-#include <exception>
+#include "MyExceptions.h"
+#include "Film.h"
+
 
 class User
 {
@@ -18,7 +17,8 @@ private:
     std::string email = "";
     std::string age = "";
     bool publisher = false;
-    
+    int balance = 0;
+
 public:
     User(int _id);
     
@@ -27,6 +27,7 @@ public:
     void set_email(std::string _email);
     void set_age(std::string _age);
     void set_publisher(bool _publisher);
+    void set_balance(int _amount);
 
     int get_id();
     std::string get_username();
@@ -34,11 +35,12 @@ public:
     std::string get_email();
     std::string get_age();
     bool get_publisher();
-
-    std::vector<int> user_film;
-    std::vector<User*> user_fallower;
-
-    long long hash(std::string s);
+    
+    std::vector<Film*> user_film;
+    std::vector<User*> user_follower;
+    std::vector<User*> user_following;
+    std::vector<Film> user_bought_film;
+    std::vector<std::string> notification;
 };
 
 #endif
